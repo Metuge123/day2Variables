e72cddbbaea34689b6a190fe873642cbcfd9c6bb
Metuge123 <152638332+Metuge123@users.noreply.github.com>
2024-05-24 05:03:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+input("What's your name?:")
+myName = input("What's your name? ")
+myAge = input("How old are you? ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit? ")
+print("OF COURSE YOU DO!")
+
+print()
+print("So you are")
+print("Metuge Bisbel")
+print("and are the ripe old age of")
+print("45")
+print("and clearly ")
+print("Happy")
+
+print()
+myvariable = input("WHO GOES THERE? ")
+print("myvariable")
+
+print()
+myGrandma = input("How's your Grandma doing? 😘 ")
+print("mygrandma is of course doing great")
+
+print()
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print("Chocolates pizza")
+print("as soon as possible!")
+
+print()
+print("Definitely not a Phishing Scam")
+print()
+input("Your Name? ")
+print("Thanks for logging in")
+print("myName")
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print("cardNumber")
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print("maidenName")
+print(
+    "was your Mum's maiden name. Not because the bank requested it or anything, honest."
+)
+
+print("""Getting to know you!
+
+You are
+Mark
+You're probably hungry for
+Burritos
+and you're definitely getting your groove on to
+Synthwave and Anything by our man Rick Astley
+living in the amazing
+Edge of reality
+""")
